29d179c2d0272c2f490d1c157a4363c7e077853d
simba-fs <simba.fs@gmail.com>
2020-06-03 21:48:37 +0800

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+const qrcode = require('qrcode');
+const ip = require('ip');
+
+console.log(process.argv);
+let port = process.argv[2] || '';
+
+a = `${ip.address()}${port ? ':' : ''}${port}`
+qrcode.toString(a).then(console.log);
+console.log(a);
